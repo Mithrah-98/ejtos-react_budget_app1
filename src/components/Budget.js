7119ad493a582b3a67699a34d3f11a9a808a4103
Logamithrah Jegakosaran <62277450+Mithrah-98@users.noreply.github.com>
2023-07-26 22:20:42 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,29 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+  const { budget, handleSetBudget } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget.toString());
+
+  const handleInputChange = (event) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value)) {
+      setNewBudget(value.toString());
+    }
+  };
+
+  const handleBlur = () => {
+    handleSetBudget(parseInt(newBudget));
+  };
+
+  return (
+    <div className='alert alert-secondary'>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>Budget: £</span>
+        <input type='number' value={newBudget} onChange={handleInputChange} onBlur={handleBlur} />
+      </div>
+    </div>
+  );
+};
+
+export default Budget;
